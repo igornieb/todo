b4f377c9558afeed058aaca030982d359872d148
igornieb <66256669+igornieb@users.noreply.github.com>
2023-01-13 13:05:25 +0100

    Update views.py
    token authentication, permissions

--- api/views.py
+++ api/views.py
@@ -5,9 +5,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from api.serializers import *
 from datetime import datetime
+from rest_framework import permissions, authentication
+
+permission_classes = (permissions.IsAuthenticated,)
+authentication_classes = [authentication.SessionAuthentication,
+                          authentication.TokenAuthentication]
 
 
 class ListActiveTasks(APIView):
+    def get_permissions(self):
+        return [permissions.IsAuthenticated()]
+
     def get_object(self):
         try:
             try:
@@ -24,38 +32,38 @@ class ListActiveTasks(APIView):
         return Response(serializer.data)
 
     def post(self, request):
-        if request.user.is_authenticated:
-            account = Account.objects.get(user=request.user)
-            serializer = TaskSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save(owner=account, is_done=False)
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+        account = Account.objects.get(user=request.user)
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=account, is_done=False)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ListDoneTasks(APIView):
+    def get_permissions(self):
+        return [permissions.IsAuthenticated()]
+
     def get_object(self):
         try:
             try:
                 account = Account.objects.get(user=self.request.user)
-            except:
+            except Account.DoesNotExist:
                 raise Http404
             return Task.objects.filter(owner=account, is_done=True)
         except Task.DoesNotExist:
             raise Http404
 
     def get(self, request):
-        if request.user.is_authenticated:
-            tasks = self.get_object()
-            serializer = TaskSerializer(tasks, many=True)
-            return Response(serializer.data)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+        tasks = self.get_object()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
 
 
 class TaskDetailView(APIView):
+    def get_permissions(self):
+        return [permissions.IsAuthenticated()]
+
     def get_object(self, pk):
         try:
             account = Account.objects.get(user=self.request.user)
@@ -64,35 +72,28 @@ class TaskDetailView(APIView):
             raise Http404
 
     def get(self, request, pk):
-        if request.user.is_authenticated:
-            task = self.get_object(pk)
-            serializer = TaskSerializer(task, many=False)
-            return Response(serializer.data)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task, many=False)
+        return Response(serializer.data)
 
     def patch(self, request, pk):
-        if request.user.is_authenticated:
-            task = self.get_object(pk)
-            account = Account.objects.get(user=self.request.user)
-            serializer = TaskSerializer(task, data=request.data)
-            if serializer.is_valid():
-                serializer.save(owner=account, date=datetime.now())
-                return Response(serializer.data)
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+        task = self.get_object(pk)
+        account = Account.objects.get(user=self.request.user)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=account, date=datetime.now())
+            return Response(serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        if request.user.is_authenticated:
             task = self.get_object(pk)
             task.delete()
             return Response(status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
 
 class AccountDetailView(APIView):
+    def get_permissions(self):
+        return [permissions.IsAuthenticated()]
+
     def get_object(self):
         if self.request.user.is_authenticated:
             return Account.objects.get(user=self.request.user)
